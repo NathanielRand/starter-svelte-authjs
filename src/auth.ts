3738ea73b45bff5d7fb5637114452baf0bdb519f
Nathaniel Rand <nathanielrand@Nathaniels-MacBook-Air.local>
2024-07-21 22:12:16 -0400

    auth: add client auth for testing in codespace (excluded server session check/set)

---
+++ src/auth.ts
@@ -0,0 +1,7 @@
+// src/auth.ts
+import { SvelteKitAuth } from "@auth/sveltekit"
+import GitHub from '@auth/sveltekit/providers/github'
+
+export const { handle, signIn } = SvelteKitAuth({
+  providers: [GitHub],
+})
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { signIn, signOut } from "@auth/sveltekit/client"
+	import { page } from "$app/stores"
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<nav>
+	<img
+	  src={$page.data.session?.user?.image ?? 'https://source.boringavatars.com/marble/120/'}
+	  alt="User Avatar"
+	  width="100"
+	  height="100"
+	/>
+</nav>
+
+<div>
+   <img src="https://placehold.co/150x150" alt="Company Logo" />
+   <button on:click={signIn}>Signin</button>
+   <button on:click={signOut}>Signout</button>
+</div> 
