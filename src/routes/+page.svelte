201912bc8dfc98d011473cfb6cc0c867d2784e2c
Nathaniel Rand <nathanielrand@Nathaniels-MacBook-Air.local>
2024-07-21 22:39:41 -0400

    test: add ! for testing auth on live

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import { page } from "$app/stores"
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit!!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <nav>
